caf615dccb9817f1abe376084ab86de26af772d7
nixpie <adriantrojanowski@protonmail.com>
2022-11-12 22:56:21 +0100

    third commit

--- todo.py
+++ todo.py
@@ -1,26 +1,26 @@
-#python todo.py --add "Wstaw ziemniaki"
-#python todo.py --list
-#python todo.py --toggle 1
+# python todo.py --add "Wstaw ziemniaki"
+# python todo.py --list
+# python todo.py --toggle 1
 
 import sqlite3
-
 from argparse import ArgumentParser
 
 parser = ArgumentParser(description='Mała aplikacja TODO')
 parser.add_argument('--install', help='Instalacja! Uwaga, wyczyści bazę danych', action='store_true')
 parser.add_argument('--add', help='Dodaj nowe zadanie')
-parser.add_argument('--list', help='Wypisz tematy do zrobienia.', action='store_true')
+parser.add_argument('--list', help='Wpisz tematy do zrobienia.', action='store_true')
 parser.add_argument('--toggle', help='Zmień status zadania')
 args = parser.parse_args()
 
 connection = sqlite3.connect('todo.db')
 cursor = connection.cursor()
 
+
 if args.install:
     print('Instalujemy program')
     cursor.execute('DROP TABLE todos')
-    cursor.execute('CREATE TABLE todo(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, is_done BOOLEAN)')
-    connection.commit()
+    cursor.execute('CREATE TABLE todos(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, is_done BOOLEAN)')
+    connection.commit
 
 if args.add is not None:
     print('Dodajemy..')
@@ -32,5 +32,4 @@ if args.toggle is not None:
     print('Przełączamy...')
 
 if args.list:
-    print('Wyswietlamy')
-
+    print('Wyświetlamy')
